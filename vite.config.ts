3b007177a83e5992654e23626adb20cebe71ee56
Ali Sami Abbas <alisame200416@gmail.com>
2026-09-19 17:43:52 +0300

    Change base path from '/marem/' to '/maryam/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/marem/',
+    base: '/maryam/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
